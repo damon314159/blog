ae143fb8633bad3ee5baaf1549ee3615845b222d
Damon <126731021+damon314159@users.noreply.github.com>
2024-09-20 18:43:27 +0100

    Fix IO to not take arguments in action

--- packages/fp/src/io/base.ts
+++ packages/fp/src/io/base.ts
@@ -1,19 +1,17 @@
 // Define unique symbols to represent the type identifier for IO
 const IO_SYMBOL: unique symbol = Symbol('IO')
 
-// An Action is an impure function that yields a value and never fails
-type Action<Args extends unknown[], Ret> = (...args: Args) => Ret
+// An Action is an impure function that yields a value of type T and never fails
+type Action<T> = () => T
 
 // IO is a wrapper that represents an impure Action that will eventually happen
-type IO<Args extends unknown[], Ret> = Readonly<{
+type IO<T> = Readonly<{
   type: typeof IO_SYMBOL
-  value: Action<Args, Ret>
+  value: Action<T>
 }>
 
 // Constructor (also known as Pure or Return)
-function Pure<Args extends unknown[], Ret>(
-  input: Action<Args, Ret>
-): IO<Args, Ret> {
+function Pure<T>(input: Action<T>): IO<T> {
   return Object.freeze({
     type: IO_SYMBOL,
     value: input,
