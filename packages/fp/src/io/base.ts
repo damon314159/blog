fc7979c67f6fd429104a42efea674082334c4f94
Damon <126731021+damon314159@users.noreply.github.com>
2024-09-20 19:06:46 +0100

    Define maps and binds for IO

--- packages/fp/src/io/base.ts
+++ packages/fp/src/io/base.ts
@@ -17,3 +17,19 @@ function Pure<T>(input: Action<T>): IO<T> {
     value: input,
   })
 }
+
+// Mappings take any value and map them without a wrapping
+type IOMapping<T, U> = (input: T) => U
+
+// Map the value that would be returned by the impure action
+function map<T, U>(mapping: IOMapping<T, U>): (input: IO<T>) => IO<U> {
+  return (input: IO<T>): IO<U> => Pure(() => mapping(input.value()))
+}
+
+// Bindings take any value and map them with a wrapping
+type IOBinding<T, U> = (input: T) => IO<U>
+
+// Binds to the value that would be returned by the impure action
+function bind<T, U>(mapping: IOBinding<T, U>): (input: IO<T>) => IO<U> {
+  return (input: IO<T>): IO<U> => Pure(() => mapping(input.value()).value())
+}
