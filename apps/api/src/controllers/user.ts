f927c1412f691202f9e6febd76fb6b531f37dfbf
Damon <126731021+damon314159@users.noreply.github.com>
2024-09-22 15:37:44 +0100

    Add validation to create user pipe

--- apps/api/src/controllers/user.ts
+++ apps/api/src/controllers/user.ts
@@ -1,18 +1,46 @@
 import { either, ioTask, pipe } from '@blog/fp'
 import { createUser, hashPassword } from '../services/user.js'
+import validate from '../validators/validate.js'
+import type { prisma } from '../bin/client.js'
 import type { Either, IO, IOTask, IOTaskBinding, Task } from '@blog/fp'
+import type { Prisma } from '@prisma/client'
+import type { Request } from 'express'
+import type { ValidationError } from 'express-validator'
+
+type CreateUserArgs = Prisma.Args<typeof prisma.user, 'create'>
 
 // This is an unfortunate workaround to missing HKT in TS
-const ioTaskEitherBind = <T, U>(
+const ioTaskEitherMap = <E, T, U>(callback: (input: T) => Either<E, U>) =>
+  ioTask.map(either.match((error: E) => either.PureL(error), callback))
+
+// This is an unfortunate workaround to missing HKT in TS
+const ioTaskEitherBind = <E, T, U>(
   callback: (
     input: T
-  ) => IO<Either<Error, U>> | Task<Either<Error, U>> | IOTask<Either<Error, U>>
+  ) => IO<Either<E, U>> | Task<Either<E, U>> | IOTask<Either<E, U>>
 ) =>
   ioTask.bind(
     either.match(
-      (error: Error) => ioTask.Pure(() => Promise.resolve(either.PureL(error))),
+      (error: E) => ioTask.Pure(() => Promise.resolve(either.PureL(error))),
       callback
-    ) as IOTaskBinding<Either<Error, T>, Either<Error, U>> // This is failure of TS inference
+    ) as IOTaskBinding<Either<E, T>, Either<E, U>> // This is failure of TS inference
   )
 
-const createUserController = pipe(hashPassword, ioTaskEitherBind(createUser))
+const extractCreateUserArgs = (
+  req: Request<object, object, CreateUserArgs['data']>
+) =>
+  Object.freeze({
+    data: {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    },
+  })
+
+const createUserController = pipe(
+  ioTaskEitherMap<ValidationError[], Request, CreateUserArgs>(
+    validate(extractCreateUserArgs)
+  ),
+  ioTaskEitherBind(hashPassword),
+  ioTaskEitherBind(createUser)
+)
