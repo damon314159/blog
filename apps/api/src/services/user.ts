1f4d49447e2961f2374288d73fd01c12902580ec
Damon <126731021+damon314159@users.noreply.github.com>
2024-09-22 15:09:34 +0100

    Do not specify that actions fail with an instance of Error

--- apps/api/src/services/user.ts
+++ apps/api/src/services/user.ts
@@ -7,27 +7,25 @@ import type { Prisma, User } from '@prisma/client'
 type CreateUserArgs = Prisma.Args<typeof prisma.user, 'create'>
 
 // Take the createUserArgs, and return a task that hashes the password
-const hashPassword: (
+const hashPassword = <E>(
   args: CreateUserArgs
-) => Task<Either<Error, CreateUserArgs>> = (args) =>
+): Task<Either<E, CreateUserArgs>> =>
   task.Pure(() =>
     hash(args.data.password, 12).then(
       (hashed: string) =>
         either.Pure(
           Object.freeze({ ...args, data: { ...args.data, password: hashed } })
         ),
-      (error: Error) => either.PureL(error)
+      (error: E) => either.PureL(error)
     )
   )
 
 // Return a task to perform the DB write
-const createUser: (args: CreateUserArgs) => IOTask<Either<Error, User>> = (
-  args
-) =>
+const createUser = <E>(args: CreateUserArgs): IOTask<Either<E, User>> =>
   ioTask.Pure(() =>
     prisma.user.create(args).then(
       (user: User) => either.Pure(Object.freeze(user)),
-      (error: Error) => either.PureL(error)
+      (error: E) => either.PureL(error)
     )
   )
 
