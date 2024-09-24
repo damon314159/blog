3b85a715571967e268b51630543d86d6385bf723
Damon <126731021+damon314159@users.noreply.github.com>
2024-09-24 17:21:28 +0100

    Delete home cooked fp package

--- packages/fp/src/task/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as task } from './base.js'
-export * from './base.js'
-export { default as ioTask } from './io-task.js'
-export * from './io-task.js'
--- packages/fp/src/utils/identity.ts
+++
@@ -1,3 +0,0 @@
-export function identity<T>(input: T): T {
-  return input
-}
