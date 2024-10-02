c06a8ab90b5d799d3840ffe855c5995bf14418ec
Damon <126731021+damon314159@users.noreply.github.com>
2024-10-02 23:40:21 +0100

    Use smart constructors in user service

--- apps/api/src/services/user.ts
+++ apps/api/src/services/user.ts
@@ -1,30 +1,31 @@
 import { compare, hash } from 'bcryptjs'
 import * as E from 'fp-ts/Either'
+import * as TE from 'fp-ts/TaskEither'
 import { prisma } from '../bin/client.js'
 import type { Prisma, User } from '@prisma/client'
-import type * as TE from 'fp-ts/TaskEither'
 
 type CreateUserArgs = Prisma.Args<typeof prisma.user, 'create'>
 
 // Take the createUserArgs, and return a task that hashes the password
-const hashPassword =
-  (args: CreateUserArgs): TE.TaskEither<Error, CreateUserArgs> =>
-  () =>
-    hash(args.data.password, 12).then(
-      (hashed: string) =>
-        E.right(
-          Object.freeze({ ...args, data: { ...args.data, password: hashed } })
-        ),
-      (error: Error) => E.left(error)
-    )
+const hashPassword = (
+  args: CreateUserArgs
+): TE.TaskEither<Error, CreateUserArgs> =>
+  TE.tryCatch(
+    () =>
+      hash(args.data.password, 12).then((hashed: string) =>
+        Object.freeze({ ...args, data: { ...args.data, password: hashed } })
+      ),
+    E.toError
+  )
 
 // Return a task to perform the DB write
-const createUser =
-  (args: CreateUserArgs): TE.TaskEither<Error, User> =>
-  () =>
-    prisma.user.create(args).then(
-      (user: User) => E.right(Object.freeze(user)),
-      (error: Error) => E.left(error)
-    )
+const createUser = (args: CreateUserArgs): TE.TaskEither<Error, User> =>
+  TE.tryCatch(
+    () =>
+      prisma.user
+        .create(args)
+        .then(Object.freeze as (user: User) => Readonly<User>),
+    E.toError
+  )
 
 export { hashPassword, createUser }
