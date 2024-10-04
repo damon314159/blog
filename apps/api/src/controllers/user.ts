b82ae6d9b7df6cb6bbfb2116866a80befca705a6
Damon <126731021+damon314159@users.noreply.github.com>
2024-10-04 22:51:38 +0100

    Add sending response into create user controller

--- apps/api/src/controllers/user.ts
+++ apps/api/src/controllers/user.ts
@@ -1,12 +1,12 @@
 import { freeze } from '@blog/functional'
 import * as E from 'fp-ts/Either'
-import { flow, pipe } from 'fp-ts/function'
+import { pipe } from 'fp-ts/function'
 import * as TE from 'fp-ts/TaskEither'
 import { createUser, hashPassword } from '../services/user.js'
 import validate from '../validators/validate.js'
 import type { prisma } from '../bin/client.js'
 import type { Prisma, User } from '@prisma/client'
-import type { Request } from 'express'
+import type { Request, Response } from 'express'
 import type { ValidationError } from 'express-validator'
 
 type CreateUserArgs = Prisma.Args<typeof prisma.user, 'create'>
@@ -25,15 +25,25 @@ const extractCreateUserArgs = (
     freeze
   )
 
-const createUserController: (
-  req: Request
-) => TE.TaskEither<ValidationError[] | Error, Readonly<User>> = flow(
-  validate,
-  E.map(extractCreateUserArgs),
-  TE.fromEither,
-  TE.flatMap(hashPassword),
-  TE.flatMap(createUser),
-  TE.map(freeze)
-)
+const sendCreateUserResponse = (res: Response) =>
+  TE.match(
+    (e: ValidationError[] | Error): void => {
+      res.status(400).json({ errors: Array.isArray(e) ? e : [e] })
+    },
+    (user: User): void => {
+      res.status(201).json({ user })
+    }
+  )
+
+const createUserController = (req: Request, res: Response): Promise<void> =>
+  pipe(
+    validate(req),
+    E.map(extractCreateUserArgs),
+    TE.fromEither,
+    TE.flatMap(hashPassword),
+    TE.flatMap(createUser),
+    TE.map(freeze),
+    sendCreateUserResponse(res)
+  )()
 
 export { createUserController }
