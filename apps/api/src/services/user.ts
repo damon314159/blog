1a39725fd7e2b47207100381e87c065c28381833
Damon <126731021+damon314159@users.noreply.github.com>
2024-10-04 21:01:23 +0100

    Refactor user service to use piped atomic functions

--- apps/api/src/services/user.ts
+++ apps/api/src/services/user.ts
@@ -1,5 +1,7 @@
+import { freeze } from '@blog/functional'
 import { compare, hash } from 'bcryptjs'
 import * as E from 'fp-ts/Either'
+import { pipe } from 'fp-ts/function'
 import * as TE from 'fp-ts/TaskEither'
 import { prisma } from '../bin/client.js'
 import type { Prisma, User } from '@prisma/client'
@@ -9,23 +11,21 @@ type CreateUserArgs = Prisma.Args<typeof prisma.user, 'create'>
 // Take the createUserArgs, and return a task that hashes the password
 const hashPassword = (
   args: CreateUserArgs
-): TE.TaskEither<Error, CreateUserArgs> =>
-  TE.tryCatch(
-    () =>
-      hash(args.data.password, 12).then((hashed: string) =>
-        Object.freeze({ ...args, data: { ...args.data, password: hashed } })
-      ),
-    E.toError
+): TE.TaskEither<Error, Readonly<CreateUserArgs>> =>
+  pipe(
+    TE.tryCatch(() => hash(args.data.password, 12), E.toError),
+    TE.map((hashed: string) =>
+      freeze({ ...args, data: { ...args.data, password: hashed } })
+    )
   )
 
 // Return a task to perform the DB write
-const createUser = (args: CreateUserArgs): TE.TaskEither<Error, User> =>
-  TE.tryCatch(
-    () =>
-      prisma.user
-        .create(args)
-        .then(Object.freeze as (user: User) => Readonly<User>),
-    E.toError
+const createUser = (
+  args: CreateUserArgs
+): TE.TaskEither<Error, Readonly<User>> =>
+  pipe(
+    TE.tryCatch(() => prisma.user.create(args), E.toError),
+    TE.map(freeze)
   )
 
 export { hashPassword, createUser }
